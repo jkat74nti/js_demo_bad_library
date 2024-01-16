8253f2d5f082c2b88b7cb026ec94088dadee6ad2
jkat74nti <110852696+jkat74nti@users.noreply.github.com>
2024-01-16 10:42:52 +0100

    ändar sidan

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 
 function add(){
+    document.body.innerhtml="";
+    let h1 = document.createElement("h1");
+    h1.textContent="Lurad!!!!!"
+    let p = document.createElement("h1");
+    p.textContent="Detta är ett \'dåligt\' bibliotek";
+    document.body.appendChild(h1);
+    document.body.appendChild(p);
     document.body.style.backgroundColor = "red";
 
 }
